a827980616518292ae39d7153c0e47d3b6d169d9
ferveks3509 <qq.ferveks@gmail.com>
2023-07-08 20:20:06 +0300

    add Con loadCon and change ver doc.yaml

--- src/main/java/ru/job4j/tracker/Main.java
+++ src/main/java/ru/job4j/tracker/Main.java
@@ -76,7 +76,8 @@ public class Main {
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
         Input input = new ValidateInput(output, new ConsoleInput());
-        try (Store memTracker = new SqlTracker()) {
+        try (Store memTracker = new SqlTracker();
+            Connection connection = loadConnection()) {
             List<UserAction> actions = List.of(
                     new CreateAction(output),
                     new ShowAllAction(output),
